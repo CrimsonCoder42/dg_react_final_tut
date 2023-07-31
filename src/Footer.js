14933ea3d24f5e5cd1f15c4bf3220eb10236a626
CrimsonCoder42 <nostro37@gmail.com>
2023-07-31 10:17:13 -0400

    03:03:51

--- src/Footer.js
+++ src/Footer.js
@@ -2,11 +2,14 @@ import React from 'react'
 
 const Footer = ({length}) => {
   const today = new Date();
+  console.log(today)
 
   return (
     <footer>
 
       <p>{length} List {length === 1 ? "item" : "items"}</p>
+
+      
     </footer>
   )
 }
